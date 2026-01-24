0f4528d7ca086336375a7d8d59999bcb62b2f246
Jayanaath <jayanaath.srirang004@gmail.com>
2026-01-24 13:39:41 +0530

    Added Experiment1

---
+++ Exp1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header'
+import Dashboard from './pages/dashboard'
+import Log from './pages/logs'
+
+function App() {
+  return (
+    <>
+      <Header heading="Eco-Track"/>
+      <Dashboard/>
+      <Log/>
+    </>
+  );
+}
+
+export default App;
---
+++ Exp1/src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header=({heading})=>{
+  return (
+      <header style={{padding:'1rem', backgroundColor:'#green',color:'white'}}>
+        <h1>{heading}</h1>
+      </header>
+  );
+};
+export default Header;
